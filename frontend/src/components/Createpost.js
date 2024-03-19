9c190157fd3c06a9425b73572d9d8f1cff3c3173
Nilatpal <143993496+Nilat105@users.noreply.github.com>
2024-03-19 13:22:43 +0530

    git push

--- frontend/src/components/Createpost.js
+++ frontend/src/components/Createpost.js
@@ -16,98 +16,101 @@ export default function Createpost() {
 
   useEffect(() => {
 
-        // saving post to mongodb
-        if (url) {
+    // saving post to mongodb
+    if (url) {
 
-          fetch("http://localhost:5000/createPost", {
-            method: "post",
-            headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + localStorage.getItem("jwt")
-          },
-            body: JSON.stringify({
-              body,
-              pic: url
-            })
-          }).then(res => res.json()) 
-            .then(data => {
-              if (data.error) {
-                notifyA(data.error)
-              } else {
-                notifyB("Successfully Posted")
-                navigate("/")
-              }
-          })
-            .catch(err => console.log(err))
+      fetch("http://localhost:5000/createPost", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + localStorage.getItem("jwt")
+        },
+        body: JSON.stringify({
+          body,
+          pic: url
+        })
+      }).then(res => res.json()) 
+        .then(data => {
+          if (data.error) {
+            notifyA(data.error)
+          } else {
+            notifyB("Successfully Posted")
+            navigate("/")
           }
-        }, [url])
+          })
+          .catch(err => console.log(err))
+          
+      }
+  }, [url])
 
 
-                 // posting image to cloudinary
-                      const postDetails = () => {
+  // posting image to cloudinary
+  const postDetails = () => {
 
-                                  console.log(body, image)
-                                               const data = new FormData()
-                                       data.append("file", image)
-                                         data.append("upload_preset", "insta-clone")
-                                                          data.append("cloud_name", "cantacloud2")
-                                          fetch("https://api.cloudinary.com/v1_1/cantacloud2/image/upload", {
-                                                                                      method: "post",
-                                                                                                   body: data
-                                                                                           }).then(res => res.json())
-                                                                                                         .then(data => setUrl(data.url))
-                                                                                       .catch(err => console.log(err))
+      console.log(body, image)
+      const data = new FormData()
+      data.append("file", image)
+      data.append("upload_preset", "insta-clone")
+      data.append("cloud_name", "cantacloud2")
+      fetch("https://api.cloudinary.com/v1_1/cantacloud2/image/upload", {
+        method: "post",
+        body: data
+      }).then(res => res.json())
+        .then(data => setUrl(data.url))
+        .catch(err => console.log(err))
 
-                                                                              console.log(url)
+      console.log(url)
                                                                                                                                                                                                                                                                 
-                                                                         console.log(url)
-
-                                                                               }
 
 
-                                                                                               const loadfile = (event) => {
-                                                                                      var output = document.getElementById("output");
-                                                                               output.src = URL.createObjectURL(event.target.files[0]);
-                                                                        output.onload = function () {
-                                                                     URL.revokeObjectURL(output.src); // free memory
-                                                                                                                     };
-                                                                                                                };
-                                                                                                return (
-                                                                                                      <div c
-                                                                                                             {/* //header */}
-                                                                                              <div className="post-header">
-                                                                                               <h4 style={{ margin: "3px auto" }}>Create New Post</h4>
-                                                                                                 <button id="post-btn" onClick={() => { postDetails() }}>Share</button>
-                                                                                                                </div>
-  {
-                                                                                      my  <div className="main-div">
-                                                                                                    <img
-                                                                                         
-                                                                                               src="https://cdn4.iconfinder.com/data/icons/ionicons/512
-                                                                                                                  />
-                                                                                                              <input
-                                                                                                              type="file"
-                                                                                                         accept="image/*"
-                                                                                                         onChange={(event) => {
-                                                                                                         loadfile(event);
-                                                                                                         setImage(event.target.files[0])
-                                                                                                               }}
-                                                                                                           />
-                                                                                                        </
-                                                                                                        {/* details */}
-                                                                                                          <div className="details">
-                                                                                                        <div className="card-header">
-                                                                                                       <div className="card-pic">
-                                                                                                       <img
-                                                                                                 src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-                                                                                                        alt=""
-                                                                                                      />
-                                                                                                                                                            <h5>Ramesh</h5>
-                                                                                                  </div>
-                                                                                                   <textarea value={body} onChange={(e) => {
-                                                                                                      setBody(e.target.value)
-                                                                                                  }} type="text" placeholder="Write a caption...."></textarea>
-                                                                                                      </div>
-                                                                                                              </div>
-                                                                                                 );
-                                                                                                     }
+  }
+
+
+  const loadfile = (event) => {
+    var output = document.getElementById("output");
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function () {
+      URL.revokeObjectURL(output.src); // free memory
+    };
+  };
+  return (
+    <div className="createPost">
+      {/* //header */}
+      <div className="post-header">
+        <h4 style={{ margin: "3px auto" }}>Create New Post</h4>                                                           
+         <button id="post-btn" onClick={() => { postDetails() }}>Share</button>
+        </div>
+  {/* image preview */}
+      <div className="main-div">
+        <img
+          id="output"
+          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          
+        />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(event) => {
+            loadfile(event);
+            setImage(event.target.files[0])
+          }}
+      <div/>                                                         </
+      {/* details */}
+      <div className="details">
+        <div className="card-header">
+          <div className="card-pic">
+            <img
+            
+              src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=6                 alt=""
+              alt=""   
+            />
+          </div>
+          <h5>Ramesh</h5>
+        </div>
+        <textarea value={body} onChange={(e) => {
+          setBody(e.target.value)
+        }} type="text" placeholder="Write a caption...."></textarea>
+      </div>
+    </div>
+  );
+}
