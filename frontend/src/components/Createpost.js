32f80b1f9d0e16598c26403b2bbdea09f91ce3f9
Nilatpal <143993496+Nilat105@users.noreply.github.com>
2024-03-20 10:58:28 +0530

    git push

--- frontend/src/components/Createpost.js
+++ frontend/src/components/Createpost.js
@@ -9,7 +9,7 @@ export default function Createpost() {
   const [url, setUrl] = useState("")
   const navigate = useNavigate()
 
-// Toast functions
+  // Toast functions
   const notifyA = (msg) => toast.error(msg)
   const notifyB = (msg) => toast.success(msg)
 
@@ -29,7 +29,7 @@ export default function Createpost() {
           body,
           pic: url
         })
-      }).then(res => res.json()) 
+      }).then(res => res.json())
         .then(data => {
           if (data.error) {
             notifyA(data.error)
@@ -37,31 +37,28 @@ export default function Createpost() {
             notifyB("Successfully Posted")
             navigate("/")
           }
-          })
-          .catch(err => console.log(err))
-          
-      }
+        })
+        .catch(err => console.log(err))
+    }
+
   }, [url])
 
 
   // posting image to cloudinary
   const postDetails = () => {
 
-      console.log(body, image)
-      const data = new FormData()
-      data.append("file", image)
-      data.append("upload_preset", "insta-clone")
-      data.append("cloud_name", "cantacloud2")
-      fetch("https://api.cloudinary.com/v1_1/cantacloud2/image/upload", {
-        method: "post",
-        body: data
-      }).then(res => res.json())
-        .then(data => setUrl(data.url))
-        .catch(err => console.log(err))
-
-      console.log(url)
-                                                                                                                                                                                                                                                                
-
+    console.log(body, image)
+    const data = new FormData()
+    data.append("file", image)
+    data.append("upload_preset", "insta-clone")
+    data.append("cloud_name", "cantacloud2")
+    fetch("https://api.cloudinary.com/v1_1/cantacloud2/image/upload", {
+      method: "post",
+      body: data
+    }).then(res => res.json())
+      .then(data => setUrl(data.url))
+      .catch(err => console.log(err))
+    console.log(url)
 
   }
 
@@ -77,15 +74,14 @@ export default function Createpost() {
     <div className="createPost">
       {/* //header */}
       <div className="post-header">
-        <h4 style={{ margin: "3px auto" }}>Create New Post</h4>                                                           
-         <button id="post-btn" onClick={() => { postDetails() }}>Share</button>
-        </div>
-  {/* image preview */}
+        <h4 style={{ margin: "3px auto" }}>Create New Post</h4>
+        <button id="post-btn" onClick={() => { postDetails() }}>Share</button>
+      </div>
+      {/* image preview */}
       <div className="main-div">
         <img
           id="output"
-          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-          
+          src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-512.png"
         />
         <input
           type="file"
@@ -94,15 +90,15 @@ export default function Createpost() {
             loadfile(event);
             setImage(event.target.files[0])
           }}
-      <div/>                                                         </
+        />
+      </div>
       {/* details */}
       <div className="details">
         <div className="card-header">
           <div className="card-pic">
             <img
-            
-              src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=6                 alt=""
-              alt=""   
+              src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
             />
           </div>
           <h5>Ramesh</h5>
@@ -113,4 +109,4 @@ export default function Createpost() {
       </div>
     </div>
   );
-}
+          }
